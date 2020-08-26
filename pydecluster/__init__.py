b53a14194e8e8774a0f2a21329bd99041cb461eb
Keurfon Luu <keurfonluu@outlook.com>
2020-08-25 23:20:02 -0700

    setup

--- pydecluster/__init__.py
+++ pydecluster/__init__.py
@@ -1,9 +1,11 @@
 from . import nearest_neighbor
 from ._catalog import Catalog
 from ._helpers import declusterize
+from .__about__ import __version__
 
 __all__ = [
     "Catalog",
     "declusterize",
     "nearest_neighbor",
+    "__version__",
 ]
