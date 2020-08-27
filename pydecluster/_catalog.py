7ad1cc0d7c940d151fbd8c33eb847bd28964ca6f
Keurfon Luu <keurfonluu@outlook.com>
2020-08-27 15:40:00 -0700

    check inputs

--- pydecluster/_catalog.py
+++ pydecluster/_catalog.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import numpy
 
 from ._helpers import declusterize
@@ -7,8 +9,25 @@ __all__ = [
 ]
 
 
+def is_arraylike(arr, size):
+    """Check input array."""
+    return isinstance(arr, (list, tuple, numpy.ndarray)) and numpy.size(arr) == size
+
+
 class Catalog:
     def __init__(self, times, eastings, northings, depths, magnitudes):
+        if not isinstance(times, (list, tuple)):
+            raise TypeError()
+        if any(not isinstance(time, datetime) for time in times):
+            raise TypeError()
+        nev = len(times)
+
+        for arr in (eastings, northings, depths, magnitudes):
+            if not is_arraylike(arr, nev):
+                raise TypeError()
+            if len(arr) != nev:
+                raise ValueError()
+
         self._times = times
         self._eastings = eastings
         self._northings = northings
