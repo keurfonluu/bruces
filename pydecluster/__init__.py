2dcaba67d42f9417118589ae6d97e981574209a8
Keurfon Luu <keurfonluu@outlook.com>
2020-08-27 20:19:33 -0700

    move function to_decimal_year to helpers

--- pydecluster/__init__.py
+++ pydecluster/__init__.py
@@ -1,11 +1,12 @@
 from . import nearest_neighbor
 from ._catalog import Catalog
-from ._helpers import declusterize
+from ._helpers import declusterize, to_decimal_year
 from .__about__ import __version__
 
 __all__ = [
     "Catalog",
     "declusterize",
     "nearest_neighbor",
+    "to_decimal_year",
     "__version__",
 ]
