444bf146b3a219dd3e59479d17782db09ab3d6a5
Keurfon Luu <keurfonluu@outlook.com>
2020-11-27 21:36:39 -0800

    remove declustering shortcuts

--- bruces/__init__.py
+++ bruces/__init__.py
@@ -1,13 +1,11 @@
 from ._catalog import Catalog
-from ._decluster import decluster, gardner_knopoff, nearest_neighbor
+from ._decluster import decluster
 from ._helpers import to_decimal_year
 from .__about__ import __version__
 
 __all__ = [
     "Catalog",
     "decluster",
-    "gardner_knopoff",
-    "nearest_neighbor",
     "to_decimal_year",
     "__version__",
 ]
--- bruces/_decluster/__init__.py
+++ bruces/_decluster/__init__.py
@@ -1,9 +1,5 @@
-from . import gardner_knopoff
-from . import nearest_neighbor
 from ._helpers import decluster
 
 __all__ = [
     "decluster",
-    "gardner_knopoff",
-    "nearest_neighbor",
 ]
