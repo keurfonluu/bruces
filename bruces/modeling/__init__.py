f2f26c7a50b9ed1fccfabf69b8b04d4656d98174
Keurfon Luu <keurfonluu@outlook.com>
2020-12-03 00:31:42 -0800

    add function magnitude_time

--- bruces/modeling/__init__.py
+++ bruces/modeling/__init__.py
@@ -1,5 +1,7 @@
+from ._magnitude_time import magnitude_time
 from ._seismicity_rate import seismicity_rate
 
 __all__ = [
+    "magnitude_time",
     "seismicity_rate",
 ]
---
+++ bruces/modeling/_magnitude_time.py
@@ -0,0 +1,87 @@
+import numpy
+
+from .._common import jitted
+from .._helpers import to_decimal_year
+
+__all__ = [
+    "magnitude_time",
+]
+
+
+@jitted
+def rate2mag(dt, r, m, bm):
+    """Generate magnitude samples for current time step."""
+    dmdt = (m[1] - m[0]) * dt
+
+    # Calculate rates per unit time per unit magnitude
+    rtm = r * bm
+
+    # Calculate total number of earthquakes
+    neq = rtm.sum() * dmdt
+
+    if neq > 0.0:
+        nm = len(m)
+
+        # Calculate cumulative distribution function
+        cdf = numpy.empty(nm, dtype=numpy.float64)
+        for i in range(nm):
+            cdf[i] = 1.0 - rtm[i:].sum() * dmdt / neq
+
+        # Sample neq magnitudes from CDF
+        mout = numpy.interp(numpy.random.rand(int(neq)), cdf, m)
+
+        return mout
+
+    else:
+        return numpy.empty(0)
+
+
+@jitted
+def rate2mag_vectorized(dt, r, m, b):
+    """Generate magnitude samples for every time steps."""
+    nm = len(m)
+    nr = len(r)
+
+    # Calculate Gutenberg-Richter rate scaling factor
+    blg10 = b * numpy.log(10.0)
+    bm = numpy.empty(nm, dtype=numpy.float64)
+    for i in range(nm):
+        bm[i] = blg10 * 10.0 ** (-b * (m[i] - m[0]))
+
+    # Loop over time steps
+    out = []
+    for i in range(nr):
+        out.append(rate2mag(dt[i], r[i], m, bm))
+
+    return out
+
+
+def magnitude_time(times, rates, m_bounds, n=50, b_value=1.0):
+    """
+    Generate earthquake magnitude-time distribution.
+
+    Parameters
+    ----------
+    times : array_like
+        Dates for every seismicity rate samples.
+    rates : array_like
+        Seismicity rates.
+    m_bounds : array_like
+        Magnitude bounds (support for CDF from which magnitude values are sampled).
+    n : int, optional, default 50
+        Number of magnitude points to define CDF.
+    b_value : scalar, optional, default 1.0
+        b-value.
+
+    Returns
+    -------
+    list
+        List of magnitude samples for every time steps.
+
+    """
+    t = to_decimal_year(times)
+    dt = numpy.diff(t)
+    dt = numpy.append(dt, dt[-1])
+    m = numpy.linspace(m_bounds[0], m_bounds[1], n)
+
+    return rate2mag_vectorized(dt, rates, m, b_value)
