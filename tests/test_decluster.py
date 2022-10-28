27d8cfd3b24d880c9963365f71ee87b24344e0e3
Keurfon Luu <keurfonluu@outlook.com>
2022-10-28 16:22:01 -0700

    add unit tests

---
+++ tests/test_decluster.py
@@ -0,0 +1,31 @@
+import helpers
+import pytest
+
+cat = helpers.comcat()
+
+
+@pytest.mark.parametrize(
+    "algorithm, decluster_kws, nev",
+    [
+        ("gardner-knopoff", {}, 154),
+        (
+            "nearest-neighbor",
+            {"d": 1.6, "w": 1.0, "eta_0": -5.1, "alpha_0": 1.5, "use_depth": True, "M": 100, "seed": 0},
+            420,
+        ),
+        (
+            "nearest-neighbor",
+            {"d": 1.6, "w": 1.0, "eta_0": -5.1, "alpha_0": 1.5, "use_depth": False, "M": 100, "seed": 0},
+            381,
+        ),
+        (
+            "reasenberg",
+            {"rfact": 10, "xmeff": 3.0, "xk": 0.5, "taumin": 1.0, "taumax": 10.0, "p": 0.95},
+            442,
+        ),
+    ],
+)
+def test_decluster(algorithm, decluster_kws, nev):
+    catd = cat.decluster(algorithm, **decluster_kws)
+
+    assert len(catd) == nev
---
+++ tests/test_modeling.py
@@ -0,0 +1,42 @@
+import datetime
+import numpy as np
+
+import bruces
+
+
+def test_seismicity_rate():
+    n = 101
+    t = np.linspace(0.0, 10.0, n)
+    s = np.ones(n) * 1.0e-3
+    s[t > 2.0] = 1.0
+    s[t > 4.0] = 1.0e-1
+
+    r = bruces.modeling.seismicity_rate(
+        times=t,
+        stress=s,
+        stress_ini=1.0e-3,
+        asigma=0.15,
+        first_step=datetime.timedelta(days=1),
+        max_step=datetime.timedelta(days=30),
+        reduce_step_factor=4.0,
+        rtol=1.0e-3,
+    )
+
+    assert np.allclose(r.sum(), 19337.18387050)
+
+
+def test_magnitude_time():
+    n = 101
+    t = np.linspace(0.0, 10.0, n)
+    r = 100.0 * np.exp(-(t - 2.0) ** 2)
+
+    mags = bruces.modeling.magnitude_time(
+        times=t,
+        rates=r,
+        m_bounds=[0.0, 2.0],
+        n=50,
+        b_value=1.0,
+        seed=0,
+    )
+
+    assert np.allclose(np.concatenate(mags).sum(), 68.91649999)
